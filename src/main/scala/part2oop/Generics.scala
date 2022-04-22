5e13ac849abf5a73ce9628af7ec0e96e2c8091fb
Beatriz Murase <beatriz.murase@cobli.co>
2022-04-21 21:14:49 -0300

    generics

--- src/main/scala/part2oop/Generics.scala
+++ src/main/scala/part2oop/Generics.scala
@@ -96,4 +96,8 @@ object Generics extends App {
      * since a dog is an animal, the list becomes a list of animals
      */
   }
+
+  /**
+   * Exercise: expand MyList (previous class, other file) to be generic
+   */
 }
